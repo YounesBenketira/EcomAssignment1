a9ea841e89470b31ebc38238999604d8844dd887
Tristan Turcotte <tristanturcotte@gmail.com>
2020-09-11 03:39:53 -0400

    Added RetrieveText method.

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,11 +11,54 @@ namespace EcomAssignment1
 {
     public partial class Default : System.Web.UI.Page
     {
-
+        SqlConnection contentConnection;
 
         protected void Page_Load(object sender, EventArgs e)
         {
-          
+            string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=\App_Data\DBContent.mdf;Integrated Security=True";
+            try
+            {
+                contentConnection = new SqlConnection(connectionString);
+                contentConnection.Open();
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }
+
+        protected string RetrieveText(int id)
+        {
+            if(contentConnection != null)
+            {
+                SqlCommand cmd = contentConnection.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM TextStorage where Id=" + id;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                if(ds.Tables[0].Rows.Count > 0)
+                    return ds.Tables[0].Rows[0]["text"].ToString();
+            }
+
+            return "";
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            if(contentConnection != null)
+            {
+                try
+                {
+                    contentConnection.Close();
+                } catch(SqlException se)
+                {
+                    System.Diagnostics.Debug.WriteLine(se.Message);
+                }
+            }
         }
     }
 }

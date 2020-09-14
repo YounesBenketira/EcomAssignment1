590e3485c70cca023ae2afc9b313c5d23883250e
Tristan Turcotte <tristanturcotte@gmail.com>
2020-09-13 22:13:45 -0400

    Added WebText class, provides database connection and text retrieval method.

---
+++ WebText.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EcomAssignment1
+{
+    public class WebText
+    {
+        private static SqlConnection connection;
+        public string Title { get; }
+        public string Header { get; }
+        public string Desc { get; }
+        public WebText(string title, string header, string body)
+        {
+            Title = title;
+            Header = header;
+            Desc = body;
+        }
+
+        public static SqlConnection TextDatabase()
+        {
+            if (connection == null)
+            {
+                // Create a connection
+                string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\DBContent.mdf;Integrated Security=True";
+                try
+                {
+                    connection = new SqlConnection(connectionString);
+                    connection.Open();
+
+                    return connection;
+                }
+                catch (Exception ex)
+                {
+                    // If connection failed in someway, alert debug console
+                    System.Diagnostics.Debug.WriteLine(ex.Message);
+                }
+            }
+
+            return connection;
+        }
+
+        public static void DisposeTextDatabase()
+        {
+            if (connection != null)
+            {
+                try
+                {
+                    connection.Close();
+                }
+                catch (SqlException se)
+                {
+                    System.Diagnostics.Debug.WriteLine(se.Message);
+                }
+            }
+        }
+        public static WebText RetrieveText(int id)
+        {
+            if(connection == null)
+            {
+                TextDatabase();
+            }
+
+            // Ensure the database connection exists before querying it
+            if (connection != null)
+            {
+                SqlCommand cmd = connection.CreateCommand();
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "SELECT * FROM TextStorage where Id=" + id;
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+
+                if (ds.Tables[0].Rows.Count > 0) // Got a result from the id, return the contained text
+                {
+                    string title = ds.Tables[0].Rows[0]["Title"].ToString();
+                    string head = ds.Tables[0].Rows[0]["Header"].ToString();
+                    string desc = ds.Tables[0].Rows[0]["Desc"].ToString();
+
+                    return new WebText(title, head, desc);
+                }
+            }
+
+            // No database connection, or no valid ID
+            return new WebText("Title","Header","Desc");
+        }
+    }
+}
